0742fb11db307ffa3de19dea76b43969940fe1e7
Thiago <thiago_viana99@hotmail.com>
2021-04-02 12:12:06 -0300

    create flatlist and modal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,35 +1,108 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import {View, Text, FlatList, Image} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  Modal,
+  Pressable,
+  Alert,
+} from 'react-native';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
 export default function Main() {
+  const [persons, setPersons] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [id, setId] = useState(1);
+
+  const load = async () => {
+    const response = await api.get(`/people/${id}`);
+    //console.log(response);
+
+    const data = {
+      name: response.data.name,
+      height: response.data.height,
+      mass: response.data.mass,
+    };
+
+    console.log('data', response.data);
+
+    setPersons([...persons, response.data]);
+    console.log(id);
+
+    if (id === 16) {
+      setId(id + 2);
+    } else {
+      setId(id + 1);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setModalVisible(true);
+    }, 5000);
+    load();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.User}>
-        <Image
-          style={styles.Avatar}
-          source={{
-            uri: 'https://www.nretnil.com/avatar/LawrenceEzekielAmos.png',
-          }}
-        />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          //Alert.alert('Modal has been closed.');
+          //setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
+              Você excedeu o tempo de uso do nosso aplicativo!{'\n\n'} Efetue o
+              pagamento para continuar utilizando nossos serviçoes.
+            </Text>
+            {/* <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable> */}
+          </View>
+        </View>
+      </Modal>
+      <FlatList
+        data={persons}
+        onEndReachedThreshold={2}
+        onEndReached={load}
+        keyExtractor={person => String(person.name)}
+        renderItem={({item}) => (
+          <View style={styles.User}>
+            <Image
+              style={styles.Avatar}
+              source={{
+                uri: 'https://www.nretnil.com/avatar/LawrenceEzekielAmos.png',
+              }}
+            />
 
-        <Text style={styles.Type}>
-          <Text style={styles.Name}>Name: </Text>
-          Luke Skywalker
-        </Text>
+            <Text style={styles.Type}>
+              <Text style={styles.Name}>Name: </Text>
+              {item.name}
+            </Text>
 
-        <Text style={styles.Type}>
-          <Text style={styles.TypeText}> Height: </Text>
-          172
-        </Text>
+            <Text style={styles.Type}>
+              <Text style={styles.TypeText}> Height: </Text>
+              {item.height}
+            </Text>
 
-        <Text style={styles.Type}>
-          <Text style={styles.TypeText}>Peso: </Text>
-          77Kg
-        </Text>
-      </View>
+            <Text style={styles.Type}>
+              <Text style={styles.TypeText}>Peso: </Text>
+              {item.mass}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -40,4 +40,31 @@ export default StyleSheet.create({
     fontWeight: 'bold',
     color: 'black',
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
 });
